c215da6d2dc0fd2c09757143c11a76c1211dfac2
Tmayt <tha617974@gmail.com>
2023-09-04 13:35:46 +0330

    test and dynamic bg

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 
 def make_lines(string):
     result = ""
-    for i, word in enumerate(string.split(' ')):
+    for word in string.split(' '):
         if len(result.split('&&&')[-1]) + len(word) > 54:
             result += f"&&& {word}"
         else:
             result += f'{word} '
 
     return result.split('&&&')
+    
+
+def make_bg(color):
+    height, width, channel = 1080, 1080, 3
+    red, green, blue = color
+    arr = np.full((height, width, channel), [red, green, blue], dtype=('uint8'))
+    plt.imsave('template.png',arr)
+    return cv2.imread('template.png')
 
 if __name__ == "__main__":
     description = '''
@@ -17,9 +28,15 @@ if __name__ == "__main__":
         text line ==> 54 chars
     '''
     print(description)
+
     slide = int(input('slide number: '))
+
+    bg_color = input('enter bg color (R,G,B) ==> ').replace('(', '').replace(')', '').split(',')
+    bg_color = bg_color if len(bg_color) == 3 else (255, 255, 255)
+    bg_color = tuple([int(i) for i in bg_color])
+    make_bg(bg_color)
     while input('continue (y/n)? ') == 'y':
-        print('-'*10)
+        print('-'*20)
         # Open the Template
         image = Image.open("template.png")
 
@@ -43,12 +60,19 @@ if __name__ == "__main__":
         position_text = [70, 140]
 
         # Specify the color of the text
-        color = (255, 255, 255)  # white color
+        color_title = input('enter title color (R,G,B) ==> ').replace('(', '').replace(')', '').split(',')
+        color_text = input('enter text color (R,G,B) ==> ').replace('(', '').replace(')', '').split(',')
+
+        color_title = color_title if len(color_title) == 3 else (0, 0, 0)
+        color_text = color_text if len(color_text) == 3 else (0, 0, 0)
+
+        color_title = tuple([int(i) for i in color_title])
+        color_text = tuple([int(i) for i in color_text])
 
         # Add the text to the image
-        draw.text(position_title, title, font=font_title, fill=color)
+        draw.text(position_title, title, font=font_title, fill=color_title)
         for text in lines:
-            draw.text(tuple(position_text), text, font=font_text, fill=color)
+            draw.text(tuple(position_text), text, font=font_text, fill=color_text)
             position_text[1] += 50
 
         # Save the merged image as a new file
--- test.py
+++ test.py
@@ -1,3 +1,20 @@
-from main import make_lines
+from main import make_lines,make_bg
+import unittest
+import cv2
 
-print(make_lines('In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia'))
+class TestStringMethods(unittest.TestCase):
+
+    def test_make_lines(self):
+        self.assertEqual(
+            make_lines('In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. Wikipedia'), 
+            ['In publishing and graphic design, Lorem ipsum is a ', ' placeholdertext commonly used to demonstrate the ', ' visualform of a document or a typeface without ', ' relyingon meaningful content. Lorem ipsum may be used ', ' asa placeholder before final copy is available. ', ' Wikipedia']
+        )
+
+    def test_make_bg(self):
+        obj1 = cv2.imencode('.jpg', make_bg((255,255,255)))[1].tobytes()
+        obj2 = cv2.imencode('.jpg', cv2.imread('test-template.png'))[1].tobytes()
+        self.assertEqual(obj1, obj2)
+
+        
+if __name__ == '__main__':
+    unittest.main()
